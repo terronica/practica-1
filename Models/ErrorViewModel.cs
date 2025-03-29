9461e19142cc787678a0787249f4035234154a00
terronica <sofiachavezmeow@gmail.com>
2025-03-28 22:43:39 -0500

    Initial commit - proyecto casa Cambio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CasaDeCambioChuquicaja.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
